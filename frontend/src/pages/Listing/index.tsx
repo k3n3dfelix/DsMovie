5eae80e35e65bcb8c7818974471cc3e2b1be2ef3
Kened Silva Felix <ksfelix1@stefanini.com>
2022-05-26 17:02:21 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,9 +1,41 @@
+import MovieCard from "components/MovieCard";
+import MovieScore from "components/MovieScore";
+import MovieStars from "components/MovieStars";
 import Pagination from "components/Pagination/Index";
 
-function Listing(){
-    return (
+
+function Listing() {
+  return (
+    <>
       <Pagination />
-    )
+
+      < div className="container">
+        <div className="row">
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+         
+        </div>
+      </div>
+
+    </>
+
+  )
 }
 
 export default Listing;
