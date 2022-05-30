8b07e59fae4129a4cf60ff81ead8626a7145bb2f
Kened Silva Felix <ksfelix1@stefanini.com>
2022-05-30 12:11:36 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,10 +1,18 @@
+import axios from "axios";
 import MovieCard from "components/MovieCard";
 import MovieScore from "components/MovieScore";
 import MovieStars from "components/MovieStars";
 import Pagination from "components/Pagination/Index";
+import { BASE_URL } from "utils/requests";
 
 
 function Listing() {
+
+  axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=id`)
+  .then(response => {
+    console.log(response.data);
+  });
+
   return (
     <>
       <Pagination />
