5c70637b162d571a8c3e59a58f26f6be679a5e2c
Kened Silva Felix <ksfelix1@stefanini.com>
2022-06-01 12:03:10 -0300

    finalizado paginação

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -34,18 +34,22 @@ function Listing() {
   }, [pageNumber]);
 
 
-    const movie = {
-        id: 1,
-        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-        title: "The Witcher",
-        count: 2,
-        score: 4.5
-    };
+    // const movie = {
+    //     id: 1,
+    //     image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
+    //     title: "The Witcher",
+    //     count: 2,
+    //     score: 4.5
+    // };
+
+  const handlePageChange = (newPageNumber : number)=>{
+    setpageNumber(newPageNumber);
+  }
 
   return (
     <>
       
-      <Pagination />
+      <Pagination page={page} onChange={handlePageChange}/>
 
       < div className="container">
         <div className="row">
